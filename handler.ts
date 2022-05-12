6628a97ff0a1b0c662e6b1e27091aadeb876d2f5
weswhitney <wesleywhitney@gmail.com>
2022-05-11 20:52:08 -0600

    feat: sending the message received from sns

--- handler.ts
+++ handler.ts
@@ -3,9 +3,10 @@ require("dotenv").config()
 import sendMessage from "./lib/push"
 
 module.exports.run = async (event: any, _context: any, _callback: any) => {
+  console.log("env ", process.env)
   console.log("Received event:", JSON.stringify(event, null, 4))
 
-  const message = event.Records[0].Sns.Message
+  var message = event.Records[0].Sns.Message
   console.log("Message received from SNS:", message)
   sendMessage(message)
 }
